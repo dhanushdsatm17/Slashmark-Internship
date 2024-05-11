b12e07f10aa5c55f58635c8144a3bb69a6e461da
dhanushdsatm17 <151713654+dhanushdsatm17@users.noreply.github.com>
2024-05-11 12:55:57 +0530

    Update Password-generator.py

--- Level-1(Basic Projects)/Password-generator.py
+++ Level-1(Basic Projects)/Password-generator.py
@@ -20,36 +20,40 @@ Security Note:
 import random
 import string
 
-def generate_password(length):
+def generate_password(lengths):
     passwords = []
-    for _ in range(length):
-        password = ""
-        for _ in range(random.randint(3, 10)):  # Randomize password length between 3 to 10 characters
-            password += random.choice(string.ascii_lowercase)
+    for length in lengths:
+        password = ''.join(random.choices(string.ascii_lowercase, k=length))
         password = replace_with_number(password)
         password = replace_with_uppercase_letter(password)
         passwords.append(password)
     return passwords
 
 def replace_with_number(password):
-    replace_index = random.randrange(len(password) // 2)
-    password = password[:replace_index] + str(random.randint(0, 9)) + password[replace_index + 1:]
+    for _ in range(random.randint(1, 2)):
+        replace_index = random.randrange(len(password) // 2)
+        password = password[:replace_index] + str(random.randint(0, 9)) + password[replace_index + 1:]
     return password
 
 def replace_with_uppercase_letter(password):
-    replace_index = random.randrange(len(password) // 2, len(password))
-    password = password[:replace_index] + password[replace_index].upper() + password[replace_index + 1:]
+    for _ in range(random.randint(1, 2)):
+        replace_index = random.randrange(len(password) // 2, len(password))
+        password = password[:replace_index] + password[replace_index].upper() + password[replace_index + 1:]
     return password
 
 def main():
     num_passwords = int(input("How many passwords do you want to generate? "))
     print(f"Generating {num_passwords} passwords")
 
-    passwords = []
+    passwords_lengths = []
+    print("Minimum length of password should be 3")
+
     for i in range(num_passwords):
         length = int(input(f"Enter the length of Password #{i+1}: "))
-        passwords.extend(generate_password(length))
+        passwords_lengths.append(max(3, length))
     
+    passwords = generate_password(passwords_lengths)
+
     for i, password in enumerate(passwords, start=1):
         print(f"Password #{i}: {password}")
 
