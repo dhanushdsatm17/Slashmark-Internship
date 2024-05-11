406bb7f35352c1f6d2896dccf7093aed2906a4fd
dhanushdsatm17 <151713654+dhanushdsatm17@users.noreply.github.com>
2024-05-11 12:52:35 +0530

    Update Password-generator.py

--- Level-1(Basic Projects)/Password-generator.py
+++ Level-1(Basic Projects)/Password-generator.py
@@ -1,3 +1,22 @@
+This Python script generates strong, random passwords that include a mix of lowercase letters, numbers, and uppercase letters.
+
+Features:
+
+1.Customizable Length: Specify the desired length for each generated password.
+2.Random Length Variation: Passwords can have a random length between 3 and 10 characters for added security.
+3.Number and Uppercase Inclusion: Ensures each password contains at least one number and one uppercase letter.
+4.Multiple Password Generation: Generate multiple passwords at once to suit your needs.
+
+Getting Started:
+
+*Save the code: Create a Python file (e.g., secure_password_generator.py) and paste the code into it.
+*Run the script: Open a terminal or command prompt, navigate to the directory containing secure_password_generator.py, and execute python secure_password_generator.py.
+
+Security Note:
+
+1.It is recommended to store passwords securely using a password manager.
+2.Do not share generated passwords with anyone.
+
 import random
 import string
 
